b5c8b20350918da87119beb2306dcf41d54c8d1d
mthmcalixto <mth.m.calixto@gmail.com>
2024-01-13 13:32:50 -0300

    0.0.14-4

--- src/components/CardsTrending/styles.tsx
+++ src/components/CardsTrending/styles.tsx
@@ -2,7 +2,7 @@ import { twc } from 'react-twc'
 
 export const Title = twc.h2`text-zinc-500 dark:text-zinc-400 font-medium text-base`
 export const Wrapper = twc.div`mt-3 relative`
-export const CardOverlay = twc.div`absolute z-20 bg-gradient-to-t from-black/[0.9] w-full h-full rounded-xl`
+export const CardOverlay = twc.div`absolute z-20 bg-gradient-to-t from-header-color dark:from-header-color-dark w-full h-full rounded-xl`
 export const CardContent = twc.div`w-full h-full`
 export const CardTitle = twc.div`absolute bottom-3 z-50 text-white px-5 py-2 font-semibold`
 export const CardAdsContent = twc.div`absolute z-30 right-4 top-2`
